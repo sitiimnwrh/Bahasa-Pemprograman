ca82fd0f354a6e45937beb4b468678ae4680b5e7
root <root@LAPTOP-GOL54139>
2024-04-28 23:02:59 +0700

    perubahan

--- pert3/tes.cpp
+++ pert3/tes.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main() {
     int a = 5;
     int b = 7;
-    int c = a+b;
+    int c = a + b;
     cout << c << endl;
     return 0;
 }
